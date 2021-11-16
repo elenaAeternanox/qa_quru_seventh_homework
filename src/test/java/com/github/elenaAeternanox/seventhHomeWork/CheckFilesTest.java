305da33fb5b6e52e239ef741713252fe11f6aad6
elenaAeternanox <antares87@mail.ru>
2021-11-16 10:54:41 +0100

    Work with zip

--- src/test/java/com/github/elenaAeternanox/seventhHomeWork/CheckFilesTest.java
+++ src/test/java/com/github/elenaAeternanox/seventhHomeWork/CheckFilesTest.java
@@ -2,6 +2,7 @@ package com.github.elenaAeternanox.seventhHomeWork;
 
 import com.codeborne.pdftest.PDF;
 import com.codeborne.xlstest.XLS;
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,15 +12,16 @@ import java.util.zip.ZipInputStream;
 
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CheckFilesTest {
 
     @Test
-    @DisplayName("Check zip file")
-    void checkZipFile() throws IOException {
+    @DisplayName("Check txt file in zip")
+    void checkTxtFileInZip() throws IOException {
         String fileName = "test.txt";
+        String fileTitle = "Txt file";
+        String fileText = "File for the check txt file in zip";
 
         ClassLoader classLoader = this.getClass().getClassLoader();
         try (InputStream is = classLoader.getResourceAsStream("test.zip");
@@ -27,6 +29,20 @@ public class CheckFilesTest {
             ZipEntry zipEntry;
             while ((zipEntry = zip.getNextEntry()) != null) {
                 assertTrue(zipEntry.getName().contains(fileName));
+                if (zipEntry.getName() == fileName) {
+                    FileOutputStream fos = new FileOutputStream("src/test/resources/" + zipEntry.getName());
+                    for (int i = zip.read(); i != -1; i = zip.read()) {
+                        fos.write(i);
+                    }
+                    fos.flush();
+                    zip.closeEntry();
+                    fos.close();
+
+                    File txt = new File("src/test/resources/" + fileName);
+                    String txtContent = IOUtils.toString(new FileReader(txt));
+                    assertTrue(txtContent.contains(fileTitle));
+                    assertTrue(txtContent.contains(fileText));
+                }
             }
         }
     }
